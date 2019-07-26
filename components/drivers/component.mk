e94b3378f611abebee15c8b8deffcfd4dcac477e
Jayvee Velayo <jayvelayo@gmail.com>
2019-07-25 22:28:51 -0700

    Changed folder structure

---
+++ components/drivers/component.mk
@@ -0,0 +1,2 @@
+COMPONENT_ADD_INCLUDEDIRS := stepper_driver/include
+COMPONENT_SRCDIRS := stepper_driver
---
+++ components/network/component.mk
@@ -0,0 +1,2 @@
+COMPONENT_ADD_INCLUDEDIRS := HTTP_get/include TCP_server/include WIFI_module/include
+COMPONENT_SRCDIRS := HTTP_get TCP_server WIFI_module
